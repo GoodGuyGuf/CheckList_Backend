8af084046c6f0008be10caace1fa9d5ae71c4a78
Ethan <ethan_gustafson@outlook.com>
2020-09-22 13:05:19 -0400

    added users#create route into routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:create, :destroy]
-  resources :users, only: [:update, :destroy] do
+  resources :users, only: [:create, :update, :destroy] do
     resources :check_lists, only: [:index, :create, :update, :destroy] do
       resources :tasks, only: [:index, :create, :update, :destroy]
     end
