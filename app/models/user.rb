835c5ab6c9ae9cbbf9b5086510f470012b8eb86c
Ethan <ethan_gustafson@outlook.com>
2020-09-22 13:07:37 -0400

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :check_lists
+
+    validates :username, :email, uniqueness: :true
+    validates :username, :email, :password, :password_confirmation, presence: :true
 end
